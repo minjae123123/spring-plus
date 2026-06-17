f09b7bdbc22c28a84d8d6671d2837e4550f5b95f
Minjae <cmj011112@gmail.com>
2026-06-17 15:01:11 +0900

    lv2-8 : QueryDSL

--- src/main/java/org/example/expert/config/PersistenceConfig.java
+++ src/main/java/org/example/expert/config/PersistenceConfig.java
@@ -1,9 +1,17 @@
 package org.example.expert.config;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @EnableJpaAuditing
 public class PersistenceConfig {
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
+        return new JPAQueryFactory(entityManager);
+    }
 }
--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -8,17 +8,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
 
-public interface TodoRepository extends JpaRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository<Todo, Long>, TodoRepositoryCustom {
 
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
-    Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
-
-    @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN t.user " +
-            "WHERE t.id = :todoId")
-    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 
     @Query(value = "SELECT t FROM Todo t LEFT JOIN FETCH t.user " +
             "WHERE (:weather IS NULL OR t.weather = :weather) " +
@@ -29,5 +21,8 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
                     "WHERE (:weather IS NULL OR t.weather = :weather) " +
                     "AND (:startDate IS NULL OR t.modifiedAt >= :startDate) " +
                     "AND (:endDate IS NULL OR t.modifiedAt < :endDate)")
-    Page<Todo> searchTodos(String weather, LocalDateTime startDateTime, LocalDateTime endDateTime, Pageable pageable);
+    Page<Todo> searchTodos(@Param("weather") String weather,
+            @Param("startDate") LocalDateTime startDateTime,
+                           @Param("endDate") LocalDateTime endDateTime,
+                           Pageable pageable);
 }
